d7cde401e734e478767f52aeca150f8c2c3eaa06
Lachlan <mindcraftlachlan@outlook.com>
2021-06-13 01:25:54 +1000

    level 1 done, added start of a wind ocndition and helpful UI

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public Mesh CompleteMagnet;
+    public MeshFilter meshfilter;
+    public ParticleSystem sparks;
+    public Magnet magnet;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            Destroy(collision.gameObject);
+            meshfilter.mesh = CompleteMagnet;
+            magnet.AttractedObjects = new GameObject[0];
+            Instantiate(sparks, transform.position, transform.rotation);
+        }
+    }
+}
