c945d81f60eabc87a463c9e7bae892836e6abce6
Lachlan <mindcraftlachlan@outlook.com>
2021-06-12 10:14:16 +1000

    magnets

---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    public GameObject[] AttractedObjects;
+    public float AttractionForce;
+
+    private void Update()
+    {
+        foreach (GameObject obj in AttractedObjects)
+        {
+            Vector3 force = transform.position - obj.transform.position;
+            force.Normalize();
+            force *= AttractionForce;
+
+            obj.GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+}
