8026f28f737c9f1451fdbf787ffc3f9bb2700e33
Lachlan <mindcraftlachlan@outlook.com>
2021-06-12 21:59:42 +1000

    level one puzzle works, needs more detail

---
+++ Assets/Lever.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public Animator anim;
+    public Interactable[] Interactables;
+
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.layer == 8)
+        {
+            anim.SetBool("On", !anim.GetBool("On"));
+            foreach(Interactable item in Interactables)
+            {
+                item.Activate();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Animator anim;
+    public virtual void Activate()
+    {
+        anim.SetBool("Activated", !anim.GetBool("Activated"));
+    }
+}
