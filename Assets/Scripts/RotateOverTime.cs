1bba18bcef2047503e2a6b759ea5762588a0299b
Lachlan <mindcraftlachlan@outlook.com>
2021-07-05 23:14:11 +1000

    level 4 new design greyboxed

---
+++ Assets/Scripts/RotateOverTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour
+{
+    public Vector3 Speeds;
+    void Update()
+    {
+        transform.Rotate(Speeds * Time.deltaTime);
+    }
+}
